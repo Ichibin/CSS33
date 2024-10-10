ee82d19b1efe30967583e47bb363e95e2b0f2cbc
Ichibin <juanernestosaavedra@outlook.com>
2024-10-10 13:14:05 -0500

    added tab index functionality

--- script.js
+++ script.js
@@ -4,35 +4,23 @@ function upDate(previewPic){
     var div = document.getElementById("image");
     var link = previewPic.src;
     // console.log(String(link));
-
     div.style.backgroundImage = "url(" + link + ")";
-    
     div.innerHTML = previewPic.alt;
-    
-    /* In this function you should 
-    1) change the url for the background image of the div with the id = "image" 
-    to the source file of the preview image
-    
-    2) Change the text  of the div with the id = "image" 
-    to the alt text of the preview image 
-    */
-
 }
 
-	function unDo(){
+function unDo(){
     // var initText = document.getElementById("image").innerHTML;
     var div = document.getElementById("image");
     // var link = previewPic.src;
-
     div.style.backgroundImage = "url('')";
     div.innerHTML = "Hover over an image below to display here.";
+}
 
-    /* In this function you should 
-    1) Update the url for the background image of the div with the id = "image" 
-    back to the orginal-image.  You can use the css code to see what that original URL was
-    
-    2) Change the text  of the div with the id = "image" 
-    back to the original text.  You can use the html code to see what that original text was
-    */
-		
+function focusing(){
+    console.log('this is focusing');
+    var imgs = document.querySelectorAll("img"); // this recieves the selectors from css files
+    for(let i = 0;i < imgs.length; i++){
+        // console.log('pog',i);
+        imgs[i].setAttribute("tabindex",i+1);
+    }
 }
